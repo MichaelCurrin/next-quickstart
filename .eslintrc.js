055e4d8062268c6e8b58bdeca62d46fc91509eac
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2022-02-25 10:47:18 +0200

    chore: update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,6 +24,7 @@ module.exports = {
       'always'
     ],
     "react/jsx-uses-react": "off",
-    "react/react-in-jsx-scope": "off"
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-key": "off"
   }
 };
